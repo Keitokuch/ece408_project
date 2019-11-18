10dd83a50a106655bbe486706f203e67bc1e36f0
jc <chenjd98@163.com>
2019-11-17 18:28:01 -0600

    milestone 3

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -8,6 +8,7 @@ namespace mxnet
 {
 namespace op
 {
+#define TILE_WIDTH 16
 
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
@@ -21,8 +22,9 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    (void)H_out; // silence declared but never referenced warning. remove this line when you start working
-    (void)W_out; // silence declared but never referenced warning. remove this line when you start working
+    int H_grid = ceil(1.0*H_out / TILE_WIDTH);
+    int W_grid = ceil(1.0*W_out / TILE_WIDTH);
+
 
 // An example use of these macros:
 // float a = y4d(0,0,0,0)
@@ -30,8 +32,20 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
 #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
 #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
-
     
+    int n, m, h, w, c, p, q;
+    n = blockIdx.x;
+    m = blockIdx.y;
+    h = (blockIdx.z / W_grid) * TILE_WIDTH + threadIdx.y;
+    w = (blockIdx.z % W_grid) * TILE_WIDTH + threadIdx.x;
+    if (h < H_out && w < W_out) {
+        float acc = 0;
+        for (c = 0; c < C; c++)
+            for (p = 0; p < K; ++p)
+                for (q = 0; q < K; ++q)
+                    acc += x4d(n, c, h + p, w + q) * k4d(m, c, p, q);
+        y4d(n, m, h, w) = acc;
+    }
 
 #undef y4d
 #undef x4d
@@ -49,17 +63,30 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
     // Use mxnet's CHECK_EQ to do assertions.
     // Remove this assertion when you do your implementation!
-    CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
 
     // Extract the tensor dimensions into B,M,C,H,W,K
     // ...
+    const int B = x.shape_[0];
+    const int M = y.shape_[1];
+    const int C = x.shape_[1];
+    const int H = x.shape_[2];
+    const int W = x.shape_[3];
+    const int K = w.shape_[3];
 
     // Set the kernel dimensions
     // dim3 gridDim(0);
     // dim3 blockDim(0);
+    int H_out = H - K + 1;
+    int W_out = W - K + 1;
+    int H_grid = ceil(1.0*H_out / TILE_WIDTH);
+    int W_grid = ceil(1.0*W_out / TILE_WIDTH);
+    int Z = H_grid * W_grid;
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
+    dim3 gridDim(B, M, Z);
+    cudaStream_t s = 0;
 
     // Call the kernel
-    // forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
