96bf39705e7e12d8390970972462cb46877f0ca3
yuanz4 <32800452+yuanz4@users.noreply.github.com>
2019-12-17 02:44:59 -0600

    add unroll_explicit and unroll_implicit

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -8,10 +8,11 @@
 #define W_out (W - K + 1)
 #define HALO_WIDTH (TILE_WIDTH + 4)
 
-/* #define ORIGINAL */
-// #define UNROLL
-#define CONSTANT
-#define SHARED
+// #define ORIGINAL
+// #define UNROLL_EXPLICIT
+#define UNROLL_IMPLICIT
+// #define CONSTANT
+// #define SHARED
 
 namespace mxnet
 {
@@ -65,27 +66,35 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 #endif
 
 
-#ifdef UNROLL
-__global__ void unroll_input(float *x_unroll, const float *x, const int b,
+#ifdef UNROLL_EXPLICIT
+#define CUDA_MAX_NUM_THREADS 1024
+#define BATCH_SIZE 1000
+__global__ void unroll_input(float *x_unroll, const float *x, const int b_index,
     const int B, const int M, const int C, const int H, const int W, const int K) {
 #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-#define x_unroll4d(i1, i0) x_unroll[(i1) * (H_out * W_out) + i0]
+#define x_unroll4d(i2, i1, i0) x_unroll[(i2) * (H_out * W_out * K * K * C) + (i1) * (H_out * W_out) + i0]
 
-    int w_unroll = blockIdx.x * blockDim.x + threadIdx.x;
-    int h_unroll = blockIdx.y * blockDim.y + threadIdx.y;
-    int c = blockIdx.y;
-    int h = w_unroll / W_out;
-    int w = w_unroll % W_out;
-    int p = threadIdx.y / K;
-    int q = threadIdx.y % K;
-    if (c < C && w_unroll < W_out * H_out && threadIdx.y < K * K)
-        x_unroll4d(h_unroll, w_unroll) = x4d(b, c, h + p, w + q);
-
+    int t = blockIdx.x * blockDim.x + threadIdx.x;
+    int W_unroll = H_out * W_out;
+    int b = b_index * BATCH_SIZE + blockIdx.y;
+    if (t < C * W_unroll) {
+        int c = t / W_unroll;
+        int w_unroll = t % W_unroll;
+        int h = w_unroll / W_out;
+        int w = w_unroll % W_out;
+        int h_base = c * K * K;
+        for (int p = 0; p < K; p++) {
+            for (int q = 0; q < K; q++) {
+                int h_unroll = h_base + p * K + q;
+                x_unroll4d(blockIdx.y, h_unroll, w_unroll) = x4d(b, c, h + p, w + q);
+            }
+        }
+    }
 #undef x4d
 #undef x_unroll4d
 }
 
-__global__ void forward_kernel(const float * __restrict__ k_unroll, const float * __restrict__ x_unroll, float * __restrict__ y, const int b,
+__global__ void forward_kernel(const float *k_unroll, const float *x_unroll, float *y, const int b_index,
     const int B, const int M, const int C, const int H, const int W, const int K) {
 #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
     __shared__ float subTileM[TILE_WIDTH][TILE_WIDTH];
@@ -106,20 +115,20 @@ __global__ void forward_kernel(const float * __restrict__ k_unroll, const float 
     int m = Row;
     int h = Col / W_out;
     int w = Col % W_out;
+    int b = b_index * BATCH_SIZE + blockIdx.z;
+    int b_base = blockIdx.z * (H_out * W_out * K * K * C);
 
     float Pvalue = 0;
-    #pragma unroll
     for (int i = 0; i < ceil(numAColumns/float(TILE_WIDTH)); i++) {
         if (Row < numARows && (i*TILE_WIDTH+tx) < numAColumns)
             subTileM[ty][tx] = k_unroll[Row*numAColumns+i*TILE_WIDTH+tx];
         else
             subTileM[ty][tx] = 0;
         if ((i*TILE_WIDTH+ty) < numBRows && Col < numBColumns)
-            subTileN[ty][tx] = x_unroll[(i*TILE_WIDTH+ty)*numBColumns+Col];
+            subTileN[ty][tx] = x_unroll[b_base+(i*TILE_WIDTH+ty)*numBColumns+Col];
         else
             subTileN[ty][tx] = 0;
         __syncthreads();
-        #pragma unroll
         for (int k = 0; k < TILE_WIDTH; k++)
             Pvalue += subTileM[ty][k] * subTileN[k][tx];
         __syncthreads();
@@ -129,7 +138,68 @@ __global__ void forward_kernel(const float * __restrict__ k_unroll, const float 
 
 #undef y4d
 }
-#endif // #ifdef UNROLL
+#endif // #ifdef UNROLL_EXPLICIT
+
+
+
+#ifdef UNROLL_IMPLICIT
+__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K) {
+#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+    __shared__ float subTileM[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float subTileN[TILE_WIDTH][TILE_WIDTH];
+
+    int numARows = M;
+    int numAColumns = C * K * K;
+    int numBRows = numAColumns;
+    int numBColumns = H_out * W_out;
+    int numCRows = numARows;
+    int numCColumns = numBColumns;
+
+    int bx = blockIdx.x, by = blockIdx.y;
+    int tx = threadIdx.x, ty = threadIdx.y;
+    int Row = by * TILE_WIDTH + ty;
+    int Col = bx * TILE_WIDTH + tx;
+
+    int b = blockIdx.z;
+    int m = Row;
+    int h = Col / W_out;
+    int w = Col % W_out;
+
+    float Pvalue = 0;
+    for (int i = 0; i < ceil(numAColumns/float(TILE_WIDTH)); i++) {
+        int temp_col = i * TILE_WIDTH + tx, temp_row = i * TILE_WIDTH + ty;
+        int K_m = Row;
+        int K_c = temp_col / (K * K);
+        int K_h = (temp_col % (K * K)) / K, K_w = (temp_col % (K * K)) % K;
+        if (Row < numARows && temp_col < numAColumns)
+            subTileM[ty][tx] = k4d(K_m, K_c, K_h, K_w);
+        else
+            subTileM[ty][tx] = 0;
+
+        int X_b = b;
+        int X_c = temp_row / (K * K);
+        int X_p = temp_row % (K * K) / K, X_q = (temp_row % (K * K)) % K;
+        int X_h = Col / W_out, X_w = Col % W_out;
+        if (temp_row < numBRows && Col < numBColumns)
+            subTileN[ty][tx] = x4d(X_b, X_c, X_h + X_p, X_w + X_q);
+        else
+            subTileN[ty][tx] = 0;
+        __syncthreads();
+        for (int k = 0; k < TILE_WIDTH; k++)
+            Pvalue += subTileM[ty][k] * subTileN[k][tx];
+        __syncthreads();
+    }
+
+    if (Row < numCRows && Col < numCColumns)
+        y4d(b, m, h, w) = Pvalue;
+
+#undef y4d
+#undef x4d
+#undef k4d
+}
+#endif // #ifdef UNROLL_IMPLICIT
 
 
 
@@ -230,22 +300,34 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
 #endif /* #ifdef ORIGINAL */
 
-#ifdef UNROLL
+
+#ifdef UNROLL_EXPLICIT
     float *x_unroll;
-    cudaMalloc((void **)&x_unroll, H_out*W_out*K*K*C*sizeof(float));
+    cudaMalloc((void **)&x_unroll, BATCH_SIZE*H_out*W_out*K*K*C*sizeof(float));
 
     // Set the kernel dimensions
-    dim3 gridDim_1(ceil(H_out*W_out/float(K*K)), C, 1);
-    dim3 blockDim_1(K*K, K*K, 1);
-    dim3 gridDim_2(ceil(H_out*W_out/float(TILE_WIDTH)), ceil(M/float(TILE_WIDTH)), 1);
+    dim3 gridDim_1(ceil(C*H_out*W_out/float(CUDA_MAX_NUM_THREADS)), BATCH_SIZE, 1);
+    dim3 blockDim_1(CUDA_MAX_NUM_THREADS, 1, 1);
+    dim3 gridDim_2(ceil(H_out*W_out/float(TILE_WIDTH)), ceil(M/float(TILE_WIDTH)), BATCH_SIZE);
     dim3 blockDim_2(TILE_WIDTH, TILE_WIDTH, 1);
 
     // Call the kernel
-    for (int b = 0; b < B; b++) {
-        unroll_input<<<gridDim_1, blockDim_1>>>(x_unroll, x.dptr_, b, B, M, C, H, W, K);
-        forward_kernel<<<gridDim_2, blockDim_2>>>(w.dptr_, x_unroll, y.dptr_, b, B, M, C, H, W, K);
+    for (int b_index = 0; b_index < B / BATCH_SIZE; b_index++) {
+        unroll_input<<<gridDim_1, blockDim_1>>>(x_unroll, x.dptr_, b_index, B, M, C, H, W, K);
+        forward_kernel<<<gridDim_2, blockDim_2>>>(w.dptr_, x_unroll, y.dptr_, b_index, B, M, C, H, W, K);
     }
-#endif /* #ifdef UNROLL  */
+#endif /* #ifdef UNROLL_EXPLICIT  */
+
+
+#ifdef UNROLL_IMPLICIT
+    // Set the kernel dimensions
+    dim3 gridDim_1(ceil(H_out*W_out/float(TILE_WIDTH)), ceil(M/float(TILE_WIDTH)), B);
+    dim3 blockDim_1(TILE_WIDTH, TILE_WIDTH, 1);
+
+    // Call the kernel
+    forward_kernel<<<gridDim_1, blockDim_1>>>(y.dptr_, x.dptr_, w.dptr_, B, M, C, H, W, K);
+#endif /* #ifdef UNROLL_IMPLICIT  */
+
 
 #ifdef SHARED
     int H_grid = ceil(1.0*H_out / TILE_WIDTH);
@@ -257,7 +339,6 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     dim3 gridDim_s(B, M, Z);
     forward_kernel<<<gridDim_s, blockDim_s, 0, s>>>(y.dptr_, x.dptr_, w.dptr_, B,M,C,H,W,K);
 #endif /* #ifdef SHARED */
-
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
